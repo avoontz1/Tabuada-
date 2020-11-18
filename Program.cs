4e7dd17e718d1900967d67c838c485f98731551e
Victor <victor.melocariati@gmail.com>
2020-11-18 11:26:03 -0300

    Tabuada finalizada

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Tabuada_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int num, tabuada, multiplicacao;
+            Console.WriteLine("Digite um número Inteiro: ");
+            num = int.Parse(Console.ReadLine());
+
+            for(tabuada = 1; tabuada<=10; tabuada++){
+                multiplicacao = num*tabuada;
+                Console.WriteLine($" {num} * {tabuada} = {multiplicacao}");
+            } 
+        }
+    }
+}
